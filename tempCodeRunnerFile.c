0e5a7f9b0610c9738391a65ce413ee41b94a9ef7
GAGAN BAHETI <gaganbaheti99@gmail.com>
2023-03-21 20:26:22 +0530

    Commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter a value of a");
+    scanf("%d",&a);
+    printf("enter a value of b");
+    scanf("%d",&b);
+
+    sum=a+b;
+
+    printf("enter a value of sum is %d",sum);
+
+
+    return 0;
+
+
+}
